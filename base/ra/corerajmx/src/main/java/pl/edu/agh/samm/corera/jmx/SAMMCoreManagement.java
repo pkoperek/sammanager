6bb9057e219674935d4601839b32fb3224ce8dfc
pkoperek <pkoperek@296ec353-4941-77d1-5aeb-86f3de74c87c>
2011-03-20 14:54:17 +0000

    refresh after interface updates
    git-svn-id: https://sammanager.googlecode.com/svn/trunk@45 296ec353-4941-77d1-5aeb-86f3de74c87c

--- base/ra/corerajmx/src/main/java/pl/edu/agh/samm/corera/jmx/SAMMCoreManagement.java
+++ base/ra/corerajmx/src/main/java/pl/edu/agh/samm/corera/jmx/SAMMCoreManagement.java
@@ -31,16 +31,16 @@ import org.slf4j.LoggerFactory;
 import pl.edu.agh.samm.common.core.IActionExecutionListener;
 import pl.edu.agh.samm.common.core.IAlarmListener;
 import pl.edu.agh.samm.common.core.ICoreManagement;
-import pl.edu.agh.samm.common.core.ILearningStageListener;
 import pl.edu.agh.samm.common.core.IResourceListener;
 import pl.edu.agh.samm.common.core.ResourceAlreadyRegisteredException;
 import pl.edu.agh.samm.common.core.ResourceNotRegisteredException;
+import pl.edu.agh.samm.common.core.Rule;
 import pl.edu.agh.samm.common.core.SLAException;
-import pl.edu.agh.samm.common.decision.IServiceLevelAgreement;
-import pl.edu.agh.samm.common.metrics.IConfiguredMetric;
+import pl.edu.agh.samm.common.metrics.IMetric;
 import pl.edu.agh.samm.common.metrics.IMetricListener;
 import pl.edu.agh.samm.common.metrics.IMetricsManagerListener;
 import pl.edu.agh.samm.common.metrics.MetricNotRunningException;
+import pl.edu.agh.samm.common.sla.IServiceLevelAgreement;
 
 /**
  * Simply delegates all calls to passed Core Management instance
@@ -52,131 +52,11 @@ import pl.edu.agh.samm.common.metrics.MetricNotRunningException;
 public class SAMMCoreManagement implements SAMMCoreManagementMBean {
 	private ICoreManagement coreManagement;
 
-	public static final String OBJECT_NAME = "pl.edu.agh.samm:type=Management";
-	private Logger logger = LoggerFactory.getLogger(SAMMCoreManagement.class.getName());
-
-	/**
-	 * @param listener
-	 * @see pl.edu.agh.samm.common.core.ICoreManagement#addActionExecutorListener(pl.edu.agh.samm.common.core.IActionExecutionListener)
-	 */
-	@Override
-	public void addActionExecutorListener(IActionExecutionListener listener) {
-		coreManagement.addActionExecutorListener(listener);
-	}
-
-	/**
-	 * @param listener
-	 * @see pl.edu.agh.samm.common.core.ICoreManagement#removeActionExecutorListener(pl.edu.agh.samm.common.core.IActionExecutionListener)
-	 */
-	@Override
-	public void removeActionExecutorListener(IActionExecutionListener listener) {
-		coreManagement.removeActionExecutorListener(listener);
-	}
-
-	/**
-	 * @return
-	 * @throws SLAException
-	 * @see pl.edu.agh.samm.common.core.ICoreManagement#retrieveCurrentSLA()
-	 */
-	@Override
-	public IServiceLevelAgreement retrieveCurrentSLA() throws SLAException {
-		return coreManagement.retrieveCurrentSLA();
-	}
-
-	/**
-	 * @param uri
-	 * @see pl.edu.agh.samm.common.core.ICoreManagement#unregisterResource(java.lang.String)
-	 */
-	@Override
-	public void unregisterResource(String uri) {
-		coreManagement.unregisterResource(uri);
-	}
-
-	/**
-	 * @param serviceLevelAgreement
-	 * @see pl.edu.agh.samm.common.core.ICoreManagement#updateSLA(pl.edu.agh.samm.common.decision.IServiceLevelAgreement)
-	 */
-	@Override
-	public void updateSLA(IServiceLevelAgreement serviceLevelAgreement) {
-		coreManagement.updateSLA(serviceLevelAgreement);
-	}
-
-	@Override
-	public boolean isSLAValidationRunning() {
-		return coreManagement.isSLAValidationRunning();
-	}
+	public static final String OBJECT_NAME = "pl.edu.agh.samm:type=CoreManagement";
+	private Logger logger = LoggerFactory.getLogger(SAMMCoreManagement.class
+			.getName());
 
 	private ObjectName objectName;
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * pl.edu.agh.samm.common.core.ICoreManagement#updateMetricPollTimeInterval
-	 * (pl.edu.agh.samm.common.metrics.IConfiguredMetric)
-	 */
-	@Override
-	public void updateMetricPollTimeInterval(IConfiguredMetric metric) throws MetricNotRunningException {
-		coreManagement.updateMetricPollTimeInterval(metric);
-	}
-
-	/**
-	 * @param listener
-	 * @see pl.edu.agh.samm.common.impl.core.ICoreManagement#addResourceListener(pl.edu.agh.samm.common.core.metrics.IResourceListener)
-	 */
-	@Override
-	public void addResourceListener(IResourceListener listener) {
-		coreManagement.addResourceListener(listener);
-	}
-
-	/**
-	 * @param metric
-	 * @param listener
-	 * @throws MetricNotRunningException
-	 * @see pl.edu.agh.samm.common.impl.core.ICoreManagement#addMetricListener(pl.edu.agh.samm.common.impl.IConfiguredMetric.IMetric,
-	 *      pl.edu.agh.samm.common.impl.metrics.IMetricListener)
-	 */
-	@Override
-	public void addRunningMetricListener(IConfiguredMetric metric, IMetricListener listener)
-			throws MetricNotRunningException {
-		coreManagement.addRunningMetricListener(metric, listener);
-	}
-
-	/**
-	 * @param listener
-	 * @see pl.edu.agh.samm.common.impl.core.ICoreManagement#addMetricsManagerListener(pl.edu.agh.samm.common.impl.metrics.IMetricsManagerListener)
-	 */
-	@Override
-	public void addRunningMetricsManagerListener(IMetricsManagerListener listener) {
-		coreManagement.addRunningMetricsManagerListener(listener);
-	}
-
-	@Override
-	public IConfiguredMetric createRunningMetricInstance(String metricURI, String resourceURI) {
-		return coreManagement.createRunningMetricInstance(metricURI, resourceURI);
-	}
-
-	@Override
-	public Collection<String> getAllRegisteredResources() {
-		return coreManagement.getAllRegisteredResources();
-	}
-
-	/**
-	 * @return the coreManagement
-	 */
-	public ICoreManagement getCoreManagement() {
-		return coreManagement;
-	}
-
-	@Override
-	public List<String> getResourceCapabilities(String uri) throws ResourceNotRegisteredException {
-		return coreManagement.getResourceCapabilities(uri);
-	}
-
-	@Override
-	public String getResourceType(String uri) {
-		return coreManagement.getResourceType(uri);
-	}
 
 	public void init() throws Exception {
 		logger.info("Starting Core Remote Access JMX...");
@@ -193,51 +73,6 @@ public class SAMMCoreManagement implements SAMMCoreManagementMBean {
 		logger.info("Stopped Core Remote Access JMX");
 	}
 
-	@Override
-	public boolean isMetricRunning(IConfiguredMetric metric) {
-		return coreManagement.isMetricRunning(metric);
-	}
-
-	@Override
-	public boolean isResourceRegistered(String uri) {
-		return coreManagement.isResourceRegistered(uri);
-	}
-
-	@Override
-	public void registerResource(String uri, String type, Map<String, Object> parameters)
-			throws ResourceAlreadyRegisteredException {
-		coreManagement.registerResource(uri, type, parameters);
-	}
-
-	/**
-	 * @param listener
-	 * @see pl.edu.agh.samm.common.impl.core.ICoreManagement#removeResourceListener(pl.edu.agh.samm.common.core.metrics.IResourceListener)
-	 */
-	@Override
-	public void removeResourceListener(IResourceListener listener) {
-		coreManagement.removeResourceListener(listener);
-	}
-
-	/**
-	 * @param metric
-	 * @param listener
-	 * @see pl.edu.agh.samm.common.impl.core.ICoreManagement#removeMetricListener(pl.edu.agh.samm.common.impl.IConfiguredMetric.IMetric,
-	 *      pl.edu.agh.samm.common.impl.metrics.IMetricListener)
-	 */
-	@Override
-	public void removeRunningMetricListener(IConfiguredMetric metric, IMetricListener listener) {
-		coreManagement.removeRunningMetricListener(metric, listener);
-	}
-
-	/**
-	 * @param listener
-	 * @see pl.edu.agh.samm.common.impl.core.ICoreManagement#removeMetricsManagerListener(pl.edu.agh.samm.common.impl.metrics.IMetricsManagerListener)
-	 */
-	@Override
-	public void removeRunningMetricsManagerListener(IMetricsManagerListener listener) {
-		coreManagement.removeRunningMetricsManagerListener(listener);
-	}
-
 	/**
 	 * @param coreManagement
 	 *            the coreManagement to set
@@ -246,83 +81,151 @@ public class SAMMCoreManagement implements SAMMCoreManagementMBean {
 		this.coreManagement = coreManagement;
 	}
 
-	/**
-	 * @param metric
-	 * @see pl.edu.agh.samm.common.impl.core.ICoreManagement#startMetric(pl.edu.agh.samm.common.impl.IConfiguredMetric.IMetric)
-	 */
-	@Override
-	public void startMetric(IConfiguredMetric metric) {
-		coreManagement.startMetric(metric);
-	}
-
-	/**
-	 * @param runningMetric
-	 * @param listeners
-	 * @see pl.edu.agh.samm.common.impl.core.ICoreManagement#startMetricAndAddRunningMetricListener(pl.edu.agh.samm.common.impl.IConfiguredMetric.IMetric,
-	 *      java.util.Collection)
-	 */
-	@Override
-	public void startMetricAndAddRunningMetricListener(IConfiguredMetric runningMetric,
-			Collection<IMetricListener> listeners) {
-		coreManagement.startMetricAndAddRunningMetricListener(runningMetric, listeners);
-	}
-
-	/**
-	 * @param runningMetric
-	 * @param listener
-	 * @see pl.edu.agh.samm.common.impl.core.ICoreManagement#startMetricAndAddRunningMetricListener(pl.edu.agh.samm.common.impl.IConfiguredMetric.IMetric,
-	 *      pl.edu.agh.samm.common.impl.metrics.IMetricListener)
-	 */
-	@Override
-	public void startMetricAndAddRunningMetricListener(IConfiguredMetric runningMetric,
-			IMetricListener listener) {
-		coreManagement.startMetricAndAddRunningMetricListener(runningMetric, listener);
-	}
-
-	/**
-	 * @param metric
-	 * @throws MetricNotRunningException
-	 * @see pl.edu.agh.samm.common.impl.core.ICoreManagement#stopMetric(pl.edu.agh.samm.common.impl.IConfiguredMetric.IMetric)
-	 */
-	@Override
-	public void stopMetric(IConfiguredMetric metric) {
-		coreManagement.stopMetric(metric);
-	}
-
 	@Override
 	public void test(String arg) {
 		logger.info("Testing SAMMCoreManagement: " + arg);
 	}
 
-	@Override
-	public void addAlarmListener(IAlarmListener listener) {
-		coreManagement.addAlarmListener(listener);
+	public void updateSLA(IServiceLevelAgreement serviceLevelAgreement) {
+		coreManagement.updateSLA(serviceLevelAgreement);
 	}
 
-	@Override
-	public void removeAlarmListener(IAlarmListener listener) {
-		coreManagement.removeAlarmListener(listener);
-	}
-
-	@Override
-	public void startSLAValidation(IServiceLevelAgreement serviceLevelAgreement) throws SLAException {
+	public void startSLAValidation(IServiceLevelAgreement serviceLevelAgreement)
+			throws SLAException {
 		coreManagement.startSLAValidation(serviceLevelAgreement);
 	}
 
-	@Override
 	public void stopSLAValidation() throws SLAException {
 		coreManagement.stopSLAValidation();
 	}
 
-	@Override
-	public void startLearning(ILearningStageListener finishListener) {
-		coreManagement.startLearning(finishListener);
+	public IServiceLevelAgreement retrieveCurrentSLA() throws SLAException {
+		return coreManagement.retrieveCurrentSLA();
 	}
 
-	@Override
+	public boolean isSLAValidationRunning() {
+		return coreManagement.isSLAValidationRunning();
+	}
+
+	public boolean isResourceRegistered(String uri) {
+		return coreManagement.isResourceRegistered(uri);
+	}
+
+	public Collection<String> getAllRegisteredResources() {
+		return coreManagement.getAllRegisteredResources();
+	}
+
+	public String getResourceType(String uri) {
+		return coreManagement.getResourceType(uri);
+	}
+
+	public List<String> getResourceCapabilities(String uri)
+			throws ResourceNotRegisteredException {
+		return coreManagement.getResourceCapabilities(uri);
+	}
+
+	public void addResourceListener(IResourceListener listener) {
+		coreManagement.addResourceListener(listener);
+	}
+
+	public void removeResourceListener(IResourceListener listener) {
+		coreManagement.removeResourceListener(listener);
+	}
+
+	public void startMetric(IMetric metric) {
+		coreManagement.startMetric(metric);
+	}
+
+	public void startMetricAndAddRunningMetricListener(IMetric runningMetric,
+			Collection<IMetricListener> listeners) {
+		coreManagement.startMetricAndAddRunningMetricListener(runningMetric,
+				listeners);
+	}
+
+	public void startMetricAndAddRunningMetricListener(IMetric runningMetric,
+			IMetricListener listener) {
+		coreManagement.startMetricAndAddRunningMetricListener(runningMetric,
+				listener);
+	}
+
+	public boolean isMetricRunning(IMetric metric) {
+		return coreManagement.isMetricRunning(metric);
+	}
+
+	public void stopMetric(IMetric metric) {
+		coreManagement.stopMetric(metric);
+	}
+
+	public void updateMetricPollTimeInterval(IMetric metric)
+			throws MetricNotRunningException {
+		coreManagement.updateMetricPollTimeInterval(metric);
+	}
+
+	public void addRunningMetricListener(IMetric metric,
+			IMetricListener listener) throws MetricNotRunningException {
+		coreManagement.addRunningMetricListener(metric, listener);
+	}
+
+	public void removeRunningMetricListener(IMetric metric,
+			IMetricListener listener) {
+		coreManagement.removeRunningMetricListener(metric, listener);
+	}
+
+	public void addRunningMetricsManagerListener(
+			IMetricsManagerListener listener) {
+		coreManagement.addRunningMetricsManagerListener(listener);
+	}
+
+	public void removeRunningMetricsManagerListener(
+			IMetricsManagerListener listener) {
+		coreManagement.removeRunningMetricsManagerListener(listener);
+	}
+
+	public IMetric createMetricInstance(String metricURI, String resourceURI) {
+		return coreManagement.createMetricInstance(metricURI, resourceURI);
+	}
+
+	public void registerResource(String uri, String type,
+			Map<String, Object> parameters)
+			throws ResourceAlreadyRegisteredException {
+		coreManagement.registerResource(uri, type, parameters);
+	}
+
+	public void unregisterResource(String uri) {
+		coreManagement.unregisterResource(uri);
+	}
+
+	public void addAlarmListener(IAlarmListener listener) {
+		coreManagement.addAlarmListener(listener);
+	}
+
+	public void removeAlarmListener(IAlarmListener listener) {
+		coreManagement.removeAlarmListener(listener);
+	}
+
 	public void addResourceParameters(String uri, Map<String, Object> parameters)
 			throws ResourceNotRegisteredException {
 		coreManagement.addResourceParameters(uri, parameters);
+	}
+
+	public void addActionExecutorListener(IActionExecutionListener listener) {
+		coreManagement.addActionExecutorListener(listener);
+	}
+
+	public void removeActionExecutorListener(IActionExecutionListener listener) {
+		coreManagement.removeActionExecutorListener(listener);
+	}
+
+	public void addRule(Rule rule) {
+		coreManagement.addRule(rule);
+	}
+
+	public void clearRules() {
+		coreManagement.clearRules();
+	}
+
+	public void removeRule(String ruleName) {
+		coreManagement.removeRule(ruleName);
 	}
 
 }
