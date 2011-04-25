571e81279e632eddb7693a5c06b0708f4ead3310
pkoperek@gmail.com <pkoperek@gmail.com@296ec353-4941-77d1-5aeb-86f3de74c87c>
2011-04-25 22:04:31 +0000

    AvgWaitTime metric
    git-svn-id: https://sammanager.googlecode.com/svn/trunk@94 296ec353-4941-77d1-5aeb-86f3de74c87c

---
+++ base/core/src/main/java/pl/edu/agh/samm/metrics/custom/AvgWaitTimeMetric.java
@@ -0,0 +1,43 @@
+package pl.edu.agh.samm.metrics.custom;
+
+import java.util.Map;
+
+import pl.edu.agh.samm.common.metrics.ICustomMetric;
+
+/**
+ * 
+ * @author koperek
+ * 
+ */
+public class AvgWaitTimeMetric implements ICustomMetric {
+
+	public static final String SERVED_COUNT_CAPABILITY = "http://www.icsr.agh.edu.pl/samm_1.owl#MasterServedCountCapability";
+	public static final String SUM_WAIT_TIME_CAPABILITY = "http://www.icsr.agh.edu.pl/samm_1.owl#MasterSumWaitTimeCapability";
+
+	private long prevServedCount = 0;
+	private long prevSumWaitTime = 0;
+
+	@Override
+	public Number computeValue(Map<String, Number> capabilitiesValues) {
+		long servedCount = capabilitiesValues.get(SERVED_COUNT_CAPABILITY)
+				.longValue();
+		long sumWaitTime = capabilitiesValues.get(SUM_WAIT_TIME_CAPABILITY)
+				.longValue();
+
+		double retVal = 0;
+
+		double servedCountDiff = servedCount - prevServedCount;
+		double sumWaitTimeDiff = sumWaitTime - prevSumWaitTime;
+
+		if (sumWaitTimeDiff == 0 || servedCountDiff == 0) {
+			return 0;
+		}
+		
+		retVal = sumWaitTimeDiff / servedCountDiff;
+
+		prevSumWaitTime = sumWaitTime;
+		prevServedCount = servedCount;
+
+		return retVal;
+	}
+}
