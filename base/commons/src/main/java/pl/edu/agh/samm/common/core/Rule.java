2a3845f30f4578d00d17349ce883195ea765ccb6
pkoperek@gmail.com <pkoperek@gmail.com@296ec353-4941-77d1-5aeb-86f3de74c87c>
2012-06-23 12:42:18 +0000

    git-svn-id: https://sammanager.googlecode.com/svn/trunk@124 296ec353-4941-77d1-5aeb-86f3de74c87c

--- base/commons/src/main/java/pl/edu/agh/samm/common/core/Rule.java
+++ base/commons/src/main/java/pl/edu/agh/samm/common/core/Rule.java
@@ -5,89 +5,92 @@ import pl.edu.agh.samm.common.action.Action;
 /**
  * Rule which defines what user expects SAMM to do when particular condition is
  * met
- * 
+ *
  * @author koperek
- * 
  */
 public class Rule {
-	private String name = null;
-	private String resourceTypeUri = null;
-	private String resourceUri = null;
-	private String metricUri = null;
-	private String condition = null;
-	private Action actionToExecute = null;
-	private String customStatement = null;
+    private String name = null;
+    private String resourceTypeUri = null;
+    private String resourceUri = null;
+    private String metricUri = null;
+    private String condition = null;
+    private Action actionToExecute = null;
+    private String customStatement = null;
 
-	@Override
-	public int hashCode() {
-		return getName().hashCode();
-	}
+    // required by xstream
+    public Rule() {
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (obj instanceof Rule) {
-			Rule o = (Rule) obj;
-			return o.getName().equals(this.getName());
-		}
-		return false;
-	}
+    @Override
+    public int hashCode() {
+        return getName().hashCode();
+    }
 
-	public Rule(String name) {
-		if (name == null) {
-			throw new NullPointerException("Rule name can't be null!");
-		}
-		this.name = name;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Rule) {
+            Rule o = (Rule) obj;
+            return o.getName().equals(this.getName());
+        }
+        return false;
+    }
 
-	public String getResourceTypeUri() {
-		return resourceTypeUri;
-	}
+    public Rule(String name) {
+        if (name == null) {
+            throw new NullPointerException("Rule name can't be null!");
+        }
+        this.name = name;
+    }
 
-	public void setResourceTypeUri(String resourceTypeUri) {
-		this.resourceTypeUri = resourceTypeUri;
-	}
+    public String getResourceTypeUri() {
+        return resourceTypeUri;
+    }
 
-	public String getResourceUri() {
-		return resourceUri;
-	}
+    public void setResourceTypeUri(String resourceTypeUri) {
+        this.resourceTypeUri = resourceTypeUri;
+    }
 
-	public void setResourceUri(String resourceUri) {
-		this.resourceUri = resourceUri;
-	}
+    public String getResourceUri() {
+        return resourceUri;
+    }
 
-	public String getMetricUri() {
-		return metricUri;
-	}
+    public void setResourceUri(String resourceUri) {
+        this.resourceUri = resourceUri;
+    }
 
-	public void setMetricUri(String metricUri) {
-		this.metricUri = metricUri;
-	}
+    public String getMetricUri() {
+        return metricUri;
+    }
 
-	public String getCondition() {
-		return condition;
-	}
+    public void setMetricUri(String metricUri) {
+        this.metricUri = metricUri;
+    }
 
-	public String getCustomStatement() {
-		return customStatement;
-	}
+    public String getCondition() {
+        return condition;
+    }
 
-	public void setCustomStatement(String customStatement) {
-		this.customStatement = customStatement;
-	}
+    public String getCustomStatement() {
+        return customStatement;
+    }
 
-	public void setCondition(String condition) {
-		this.condition = condition;
-	}
+    public void setCustomStatement(String customStatement) {
+        this.customStatement = customStatement;
+    }
 
-	public Action getActionToExecute() {
-		return actionToExecute;
-	}
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
 
-	public void setActionToExecute(Action actionToExecute) {
-		this.actionToExecute = actionToExecute;
-	}
+    public Action getActionToExecute() {
+        return actionToExecute;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setActionToExecute(Action actionToExecute) {
+        this.actionToExecute = actionToExecute;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
