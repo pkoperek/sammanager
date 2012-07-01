79bd6ad8a2b0d1902dd4855889976c0f21fbf75e
pkoperek@gmail.com <pkoperek@gmail.com@296ec353-4941-77d1-5aeb-86f3de74c87c>
2012-07-01 13:11:10 +0000

    #240: testy dla jmx ta
    git-svn-id: https://sammanager.googlecode.com/svn/trunk@148 296ec353-4941-77d1-5aeb-86f3de74c87c

--- base/transportadapters/jmx_ta/src/main/java/pl/edu/agh/samm/tadapter/jmx/JMXAdapterConfigurator.java
+++ base/transportadapters/jmx_ta/src/main/java/pl/edu/agh/samm/tadapter/jmx/JMXAdapterConfigurator.java
@@ -34,164 +34,17 @@ import java.util.Properties;
 public class JMXAdapterConfigurator {
 
 	public static final String ENV_PROPERTIES_FILE = "jmxAdapter.propertiesFile";
+	private Properties mappings;
+    private Properties defaultMappings;
 
-	private static final Properties DEFAULT_PROPERTIES = new Properties();
-	private Properties properties;
-
-	public JMXAdapterConfigurator() {
-		// master application
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"masterapplication.instance.name",
-				"pl.edu.agh.samm.test:type=ExpressionGenerator|Name");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"masterapplication.capability.MasterQueueLengthCapability",
-				"pl.edu.agh.samm.test:type=ExpressionGenerator|QueueLength");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"masterapplication.capability.MasterServedCountCapability",
-				"pl.edu.agh.samm.test:type=ExpressionGenerator|ServedCount");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"masterapplication.capability.MasterSumWaitTimeCapability",
-				"pl.edu.agh.samm.test:type=ExpressionGenerator|SumWaitTime");		
-
-		// slave application
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"slaveapplication.instance.prefix", "SlaveNodeApp_");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"slaveapplication.instance.name",
-				"pl.edu.agh.samm.test:type=Slave|Id");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"slaveapplication.capability.SlaveQueueLengthCapability",
-				"pl.edu.agh.samm.test:type=Slave|QueueLength");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"slaveapplication.capability.SlaveProcessedCountCapability",
-				"pl.edu.agh.samm.test:type=Slave|ProcessedCount");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"slaveapplication.capability.SlaveSumProcessingTimeCapability",
-				"pl.edu.agh.samm.test:type=Slave|SumProcessingTime");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"slaveapplication.capability.SlaveAvgProcessingTimeCapability",
-				"pl.edu.agh.samm.test:type=Slave|AvgProcessTime");
-
-		// general
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put("jvm.instance.prefix",
-		"JVM_");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put("jvm.instance.name",
-				"java.lang:type=Runtime|Name");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"operatingsystem.instance.name",
-				"java.lang:type=OperatingSystem|Name");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put("cpu.instance.name",
-				"java.lang:type=OperatingSystem|Arch");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put("cpu.instance.prefix",
-				"cpu_");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"harddisk.instance.prefix", "hd1");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put("memory.instance.prefix",
-				"currentMemory");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put("thread.instance.name",
-				"java.lang:type=Threading|AllThreadIds");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put("thread.instance.prefix",
-				"Thread_");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put("tomcat.instance.name",
-				"Catalina:type=Engine|name");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put("tomcat.instance.prefix",
-				"");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES
-				.put("tomcat.capability.RequestCountTypeCapability",
-						"Catalina:type=GlobalRequestProcessor,name=http-8080|requestCount");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES
-				.put("tomcat.capability.ProcessingTimeTypeCapability",
-						"Catalina:type=GlobalRequestProcessor,name=http-8080|processingTime");
-
-		// jvm
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"jvm.capability.UptimeTypeCapability",
-				"java.lang:type=Runtime|Uptime");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"jvm.capability.TotalUnloadedClassesTypeCapability",
-				"java.lang:type=ClassLoading|UnloadedClassCount");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"jvm.capability.TotalLoadedClassesTypeCapability",
-				"java.lang:type=ClassLoading|TotalLoadedClassCount");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"jvm.capability.TotalStartedThreadsCountTypeCapability",
-				"java.lang:type=Threading|TotalStartedThreadCount");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"jvm.capability.LiveThreadsCountTypeCapability",
-				"java.lang:type=Threading|ThreadCount");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"jvm.capability.CurrentLoadedClassesTypeCapability",
-				"java.lang:type=ClassLoading|LoadedClassCount");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"jvm.capability.HeapUsageTypeCapability",
-				"java.lang:type=Memory|HeapMemoryUsage|used");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"jvm.capability.NonHeapUsageTypeCapability",
-				"java.lang:type=Memory|NonHeapMemoryUsage|used");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"jvm.capability.PermMemoryUsageTypeCapability",
-				"java.lang:type=MemoryPool,name=Perm Gen|Usage|used");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES
-				.put("jvm.capability.MinorGCTotalCountTypeCapability",
-						"java.lang:type=GarbageCollector,name=PS Scavenge|CollectionCount");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES
-				.put("jvm.capability.MinorGCTotalTimeTypeCapability",
-						"java.lang:type=GarbageCollector,name=PS Scavenge|CollectionTime");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES
-				.put("jvm.capability.MajorGCTotalCountTypeCapability",
-						"java.lang:type=GarbageCollector,name=PS MarkSweep|CollectionCount");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES
-				.put("jvm.capability.MajorGCTotalTimeTypeCapability",
-						"java.lang:type=GarbageCollector,name=PS MarkSweep|CollectionTime");
-
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"jvm.capability.TotalCompilationTimeTypeCapability",
-				"java.lang:type=Compilation|TotalCompilationTime");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"jvm.capability.OpenFileDescriptorCountTypeCapability",
-				"java.lang:type=OperatingSystem|OpenFileDescriptorCount");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"jvm.capability.JVMTotalCPUTimeTypeCapability",
-				"java.lang:type=OperatingSystem|ProcessCpuTime");
-
-		// thread
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"thread.capability.ThreadCPUTimeTypeCapability",
-				"java.lang:type=Threading|getThreadCpuTime(@instance),long");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"thread.capability.ThreadUserTimeTypeCapability",
-				"java.lang:type=Threading|getThreadUserTime(@instance),long");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES
-				.put("thread.capability.ThreadBlockedCountTypeCapability",
-						"java.lang:type=Threading|getThreadInfo(@instance),long|blockedCount");
-
-		// operating system
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES
-				.put("operatingsystem.capability.AvailableVirtualMemoryTypeCapability",
-						"java.lang:type=OperatingSystem|FreeSwapSpaceSize");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"operatingsystem.capability.UptimeTypeCapability",
-				"linuxMonitoringExtension:type=SystemInformation|Uptime");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"operatingsystem.capability.Load15minTypeCapability",
-				"linuxMonitoringExtension:type=SystemInformation|L15m");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"operatingsystem.capability.Load5minTypeCapability",
-				"linuxMonitoringExtension:type=SystemInformation|L5m");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES.put(
-				"operatingsystem.capability.Load1minTypeCapability",
-				"linuxMonitoringExtension:type=SystemInformation|L1m");
-		JMXAdapterConfigurator.DEFAULT_PROPERTIES
-				.put("operatingsystem.capability.AvailablePhysicalMemoryTypeCapability",
-						"java.lang:type=OperatingSystem|FreePhysicalMemorySize");
-	}
-
+    private String defaultPropertiesFile;
+    
 	public void init() throws FileNotFoundException, IOException {
 		String propertiesFileName = System.getProperty(ENV_PROPERTIES_FILE);
 		if (propertiesFileName != null) {
 			loadConfiguration(propertiesFileName);
 		} else {
-			properties = DEFAULT_PROPERTIES;
+			mappings = defaultMappings;
 		}
 	}
 
@@ -200,11 +53,11 @@ public class JMXAdapterConfigurator {
 	 */
 	public void loadConfiguration(final File file)
 			throws FileNotFoundException, IOException {
-		properties = new Properties();
+		mappings = new Properties();
 		InputStream inputStream = null;
 		try {
 			inputStream = new FileInputStream(file);
-			properties.load(inputStream);
+			mappings.load(inputStream);
 		} finally {
 			if (inputStream != null)
 				inputStream.close();
@@ -218,7 +71,14 @@ public class JMXAdapterConfigurator {
 	}
 
 	public String getProperty(String key) {
-		return properties.getProperty(key);
+		return mappings.getProperty(key);
 	}
 
+    public Properties getDefaultMappings() {
+        return defaultMappings;
+    }
+
+    public void setDefaultMappings(Properties defaultMappings) {
+        this.defaultMappings = defaultMappings;
+    }
 }
---
+++ base/transportadapters/jmx_ta/src/test/java/pl/edu/agh/samm/tadapter/jmx/JMXAdapterConfiguratorTest.java
@@ -0,0 +1,57 @@
+package pl.edu.agh.samm.tadapter.jmx;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: koperek
+ * Date: 01.07.12
+ * Time: 14:17
+ */
+
+public class JMXAdapterConfiguratorTest {
+    public static final String VALUE = "val";
+    public static final String PROPERTY = "prop";
+    private JMXAdapterConfigurator jmxAdapterConfigurator;
+
+    @Before
+    public void setUp() throws Exception {
+        jmxAdapterConfigurator = new JMXAdapterConfigurator();
+    }
+
+    @Test
+    public void testInit() throws Exception {
+        // Given
+        File tempFile = File.createTempFile("pfix","sfix");
+        tempFile.deleteOnExit();
+        storeProperties(tempFile);
+
+        System.setProperty(JMXAdapterConfigurator.ENV_PROPERTIES_FILE, tempFile.getAbsolutePath());
+
+        // When
+        jmxAdapterConfigurator.init();
+
+        // Then
+        assertEquals(VALUE, jmxAdapterConfigurator.getProperty(PROPERTY));
+    }
+
+    private void storeProperties(File tempFile) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(tempFile);
+        storeProperties(fileOutputStream);
+        fileOutputStream.close();
+    }
+
+    private void storeProperties(FileOutputStream fileOutputStream) throws IOException {
+        Properties properties = new Properties();
+        properties.setProperty(PROPERTY, VALUE);
+        properties.store(fileOutputStream, "comment");
+    }
+}
