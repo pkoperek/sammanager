f068c5b4c31ead19dd36c994192260c82501a462
pkoperek@gmail.com <pkoperek@gmail.com@296ec353-4941-77d1-5aeb-86f3de74c87c>
2012-08-18 21:00:51 +0000

    #254: cleanup
    git-svn-id: https://sammanager.googlecode.com/svn/trunk@193 296ec353-4941-77d1-5aeb-86f3de74c87c

--- parent/src/main/scripts/run.sh
+++ parent/src/main/scripts/run.sh
@@ -18,7 +18,7 @@
 
 cd osgi
 
-CODEBASE="file://$(pwd)/core-0.1-SNAPSHOT.jar file://$(pwd)/commons-0.1-SNAPSHOT.jar"
+CODEBASE="file://$(pwd)/core-0.1-SNAPSHOT.jar file://$(pwd)/api-0.1-SNAPSHOT.jar"
 java -Dcom.sun.management.jmxremote.port=12345  -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dosgi.compatibility.bootdelegation=true -Djava.security.manager=java.rmi.RMISecurityManager -Djava.security.policy=../server.policy -Djava.rmi.server.codebase="$CODEBASE" -jar org.eclipse.osgi-3.6.0.v20100517.jar $@ &
 PID=$!
 echo $PID > samm.pid
