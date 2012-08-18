34e6e88294e552dccf8d40805811e509e682e2f7
pkoperek@gmail.com <pkoperek@gmail.com@296ec353-4941-77d1-5aeb-86f3de74c87c>
2012-08-18 20:55:43 +0000

    #254: cleanup
    git-svn-id: https://sammanager.googlecode.com/svn/trunk@192 296ec353-4941-77d1-5aeb-86f3de74c87c

--- base/core/src/main/java/pl/edu/agh/samm/core/ActionExecutionDBListener.java
+++ base/core/src/main/java/pl/edu/agh/samm/core/ActionExecutionDBListener.java
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with SAMM.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package pl.edu.agh.samm.core;
 
 import pl.edu.agh.samm.api.action.ActionExecution;
