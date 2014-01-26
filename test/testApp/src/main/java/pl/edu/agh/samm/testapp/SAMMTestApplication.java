38be179cfbc90b1aacad7cb96b7ba126d1d05ec0
Pawel Koperek <pkoperek@gmail.com>
2014-01-26 22:55:22 +0100

    controlling generation of expressions with web ui

--- test/testApp/src/main/java/pl/edu/agh/samm/testapp/SAMMTestApplication.java
+++ test/testApp/src/main/java/pl/edu/agh/samm/testapp/SAMMTestApplication.java
@@ -18,30 +18,79 @@ package pl.edu.agh.samm.testapp;
 import com.vaadin.Application;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.TextArea;
 import com.vaadin.ui.Window;
+import pl.edu.agh.samm.testapp.core.ExpressionGenerator;
 
 /**
  * The Application's "main" class
  */
 @SuppressWarnings("serial")
-public class SAMMTestApplication extends Application
-{
-    private Window window;
+public class SAMMTestApplication extends Application {
+
+    private static final String START_WORKLOAD = "Start workload";
+    private static final String STOP_WORKLOAD = "Stop workload";
+    private static final int MAX_LVL = 10;
+
+    private Window mainWindow;
+
+    private Thread expressionGeneratorThread;
+    private ExpressionGenerator expressionGenerator;
+    private TextArea generationLogTextArea;
 
     @Override
-    public void init()
-    {
-        window = new Window("SAMM Test Application");
-        setMainWindow(window);
-        Button button = new Button("Click Me");
-        button.addListener(new Button.ClickListener() {
+    public void init() {
+        expressionGenerator = new ExpressionGenerator(MAX_LVL);
+        expressionGeneratorThread = new Thread(expressionGenerator);
+        expressionGeneratorThread.start();
+
+        mainWindow = new Window("SAMM Test Application");
+
+        mainWindow.addComponent(createGenerationControlPanel());
+
+        setMainWindow(mainWindow);
+    }
+
+    private Panel createGenerationControlPanel() {
+        final Panel generationControlPanel = new Panel("Data generation control:");
+
+        Button generationControlButton = createGenerationControlButton();
+        generationLogTextArea = createGenerationLogTextArea();
+        generationControlPanel.addComponent(generationControlButton);
+        generationControlPanel.addComponent(generationLogTextArea);
+
+        return generationControlPanel;
+    }
+
+    private void stopGenerating() {
+        expressionGenerator.stopGeneration();
+    }
+
+    private void startGenerating() {
+        expressionGenerator.startGeneration();
+    }
+
+    private TextArea createGenerationLogTextArea() {
+        TextArea log = new TextArea();
+        log.setWidth("100%");
+        log.setHeight("100%");
+        return log;
+    }
+
+    private Button createGenerationControlButton() {
+        final Button generationControl = new Button(START_WORKLOAD);
+        generationControl.addListener(new Button.ClickListener() {
             public void buttonClick(ClickEvent event) {
-                window.addComponent(new Label("Thank you for clicking"));
+                if (generationControl.getCaption().equals(START_WORKLOAD)) {
+                    startGenerating();
+                    generationControl.setCaption(STOP_WORKLOAD);
+                } else {
+                    stopGenerating();
+                    generationControl.setCaption(START_WORKLOAD);
+                }
             }
         });
-        window.addComponent(button);
-        
+        return generationControl;
     }
-    
 }
