f006ca7d44a002eb79ad4418180ca49f111e1d10
Pawel Koperek <pkoperek@gmail.com>
2014-03-11 21:00:26 +0000

    input validation in start script

--- parent/src/main/scripts/start.sh
+++ parent/src/main/scripts/start.sh
@@ -16,6 +16,12 @@
 # along with SAMM.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+CONFIG_FILE=$1
+if [ -z "$CONFIG_FILE" ]; then
+    echo "No configuration file specified! Aborting..."
+    exit 1
+fi
+
 CODEBASE="file://$(pwd)/core-0.1-SNAPSHOT.jar file://$(pwd)/api-0.1-SNAPSHOT.jar"
 java -DconfigFile=$1 -Dosgi.compatibility.bootdelegation=true -Djava.security.manager=java.rmi.RMISecurityManager -Djava.security.policy=server.policy -Djava.rmi.server.codebase="$CODEBASE" -jar osgi/org.eclipse.osgi-3.6.0.v20100517.jar &
 PID=$!
